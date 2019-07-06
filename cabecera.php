ee74ae7db270db17df8f5f81310cd4daebf7c1df
cseiio <48368842+cseiio@users.noreply.github.com>
2019-07-05 19:32:51 -0500

    nuevos cambios

--- cabecera.php
+++ cabecera.php
@@ -3,12 +3,7 @@
 
 <head>
 
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta http-equiv="Content-Language" content="es">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
+  ahora toc toc
 
 
 
